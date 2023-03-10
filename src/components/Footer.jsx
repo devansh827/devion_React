bcae9403bf508ce4d7a7dd492e2f1d33941e44fa
devanshs288 <devanshs288.ds@gmail.com>
2023-03-10 16:27:01 +0530

    first version

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+
+    
+    <div>
+        <h1>Devion</h1>
+        <p>@all right reserved</p>
+    </div>
+
+     <div>
+        <h5>Follow us</h5>
+        <div>
+            <a href="https://www.instagram.com/devansh.ds/" target={"blank"}>Youtube</a>
+            <a href="https://www.instagram.com/devansh.ds/"target={"blank"}>Instagram</a>
+            <a href="https://www.instagram.com/devansh.ds/"target={"blank"}>Github</a>
+        </div>
+     </div>
+    </footer>
+  )
+}
+
+export default Footer
